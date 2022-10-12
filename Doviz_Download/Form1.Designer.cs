e4ca2db4364837165685e0bea49408d879ef2b6d
FARUK UZUN <farukbirkanuzun@gmail.com>
2022-10-12 13:15:53 +0300

    Add project files.

---
+++ Doviz_Download/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Doviz_Download
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbldolar = new System.Windows.Forms.Label();
+            this.lbleuro = new System.Windows.Forms.Label();
+            this.lblsterlin = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbldolar
+            // 
+            this.lbldolar.AutoSize = true;
+            this.lbldolar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbldolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbldolar.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbldolar.Location = new System.Drawing.Point(137, 72);
+            this.lbldolar.Name = "lbldolar";
+            this.lbldolar.Size = new System.Drawing.Size(0, 16);
+            this.lbldolar.TabIndex = 3;
+            // 
+            // lbleuro
+            // 
+            this.lbleuro.AutoSize = true;
+            this.lbleuro.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbleuro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbleuro.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbleuro.Location = new System.Drawing.Point(137, 132);
+            this.lbleuro.Name = "lbleuro";
+            this.lbleuro.Size = new System.Drawing.Size(0, 16);
+            this.lbleuro.TabIndex = 3;
+            // 
+            // lblsterlin
+            // 
+            this.lblsterlin.AutoSize = true;
+            this.lblsterlin.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblsterlin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsterlin.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblsterlin.Location = new System.Drawing.Point(137, 203);
+            this.lblsterlin.Name = "lblsterlin";
+            this.lblsterlin.Size = new System.Drawing.Size(0, 16);
+            this.lblsterlin.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(12, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "DOLAR";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(12, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 16);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "EURO";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label7.Location = new System.Drawing.Point(12, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 16);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "STERLIN";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(225, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(590, 178);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(225, 32);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(315, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(117, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Başlama Tarihi : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(825, 248);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblsterlin);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbleuro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbldolar);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Döviz Kur Indir";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbldolar;
+        private System.Windows.Forms.Label lbleuro;
+        private System.Windows.Forms.Label lblsterlin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Doviz_Download/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Doviz_Download
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+       
+            try
+            {
+                XmlDocument xmlVerisi = new XmlDocument();
+
+                DateTime dt1;
+               
+                dt1 = dateTimePicker1.Value.Date;
+               
+                try
+                {
+                   
+
+                   if (dateTimePicker1.Value.DayOfWeek == DayOfWeek.Saturday || dateTimePicker1.Value.DayOfWeek == DayOfWeek.Sunday)
+                    {
+
+                        MessageBox.Show("Hafta Sonu Veri Yoktur ... ");
+                    }
+                    else
+                    {
+                        if (dateTimePicker1.Value.Day < 10 || dateTimePicker1.Value.Month < 10)
+                        {
+                            xmlVerisi.Load(@"https://www.tcmb.gov.tr/kurlar/" + dateTimePicker1.Value.Year + "" + dateTimePicker1.Value.Month.ToString().PadLeft(2, '0') + "/" + dateTimePicker1.Value.Day.ToString().PadLeft(2, '0') + "" + dateTimePicker1.Value.Month.ToString().PadLeft(2, '0') + "" + dateTimePicker1.Value.Year + ".xml");
+
+                            decimal dolar = Convert.ToDecimal(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "USD")).InnerText.Replace('.', ','));
+                            decimal euro = Convert.ToDecimal(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "EUR")).InnerText.Replace('.', ','));
+                            decimal sterlin = Convert.ToDecimal(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "GBP")).InnerText.Replace('.', ','));
+
+                            lbldolar.Text = dolar.ToString();
+                            lbleuro.Text = euro.ToString();
+                            lblsterlin.Text = sterlin.ToString();
+
+                            tablo.Rows.Add(dateTimePicker1.Value.ToShortDateString(), lbldolar.Text, lbleuro.Text, lblsterlin.Text);
+
+
+                            dataGridView1.DataSource = tablo;
+                        }
+                        else
+                        {
+                            xmlVerisi.Load(@"https://www.tcmb.gov.tr/kurlar/" + dateTimePicker1.Value.Year + "" + dateTimePicker1.Value.Month + "/" + dateTimePicker1.Value.Day + "" + dateTimePicker1.Value.Month + "" + dateTimePicker1.Value.Year + ".xml");
+
+                        }
+                    }
+
+
+
+
+
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show(xmlVerisi.ToString());
+                }
+
+
+            }
+            catch (XmlException xml)
+            {
+
+                MessageBox.Show(xml.ToString());
+            }
+        }
+        DataTable tablo = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tablo.Columns.Add("Tarih", typeof(string));
+           
+            tablo.Columns.Add("DOLAR", typeof(string));
+            tablo.Columns.Add("EURO", typeof(string));
+            tablo.Columns.Add("STERLIN", typeof(string));
+
+            dataGridView1.DataSource = tablo;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
